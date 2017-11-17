875c7101b5ac4b84df1db74cbbab55ac4ae37e14
Ivan Rep <ivan.rep2@gmail.com>
2017-11-17 15:21:46 +0100

    Initial commit

---
+++ SwiftDoodle/SwiftDoodle.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDoodle.h
+//  SwiftDoodle
+//
+//  Created by Ivan Rep on 17/11/2017.
+//  Copyright © 2017 Ivan Rep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDoodle.
+FOUNDATION_EXPORT double SwiftDoodleVersionNumber;
+
+//! Project version string for SwiftDoodle.
+FOUNDATION_EXPORT const unsigned char SwiftDoodleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDoodle/PublicHeader.h>
+
+
