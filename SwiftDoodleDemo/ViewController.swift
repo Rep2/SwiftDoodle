ef05601be2016205a1313ec066d34e45fbe794d7
Ivan Rep <ivan.rep2@gmail.com>
2017-11-17 15:23:58 +0100

    Added demo app

---
+++ SwiftDoodleDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDoodleDemo
+//
+//  Created by Ivan Rep on 17/11/2017.
+//  Copyright © 2017 Ivan Rep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
