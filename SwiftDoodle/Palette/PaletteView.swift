4493d492d3e66b38a74056823b5d362c63eddf05
Ivan Rep <ivan.rep2@gmail.com>
2018-02-19 00:17:50 +0100

    Brush size preview

--- SwiftDoodle/Palette/PaletteView.swift
+++ SwiftDoodle/Palette/PaletteView.swift
@@ -10,6 +10,18 @@ public final class PaletteView: UIView, FromNib {
     @IBOutlet fileprivate weak var drawWidthSlider: UISlider!
     @IBOutlet fileprivate weak var colorPickerImageView: UICollectionView!
 
+    lazy var brushSizePreview: UIView = {
+        let view = UIView()
+
+        view.layer.cornerRadius = 25
+        view.layer.masksToBounds = true
+        view.isHidden = true
+
+        self.addSubview(view)
+
+        return view
+    }()
+
     var viewModel: PaletteViewModel?
 
     public weak var eventHandler: PaletteViewEventHandler?
@@ -29,6 +41,34 @@ public final class PaletteView: UIView, FromNib {
         layer.cornerRadius = 10
 
         colorPickerImageView.registerNib(cellType: RoundImageCollectionViewCell.self)
+
+        brushSizePreview.snp.makeConstraints { make in
+            make.height.width.equalTo(50)
+            make.right.equalTo(self.snp.left).offset(-16)
+            make.bottom.equalTo(self.snp.bottom).offset(-16)
+        }
+
+        drawWidthSlider
+            .addTarget(self, action: #selector(onSliderValChanged(slider:event:)), for: .valueChanged)
+    }
+
+    @objc
+    func onSliderValChanged(slider: UISlider, event: UIEvent) {
+        if let touchEvent = event.allTouches?.first {
+            switch touchEvent.phase {
+            case .began:
+                guard let viewModel = viewModel else { return }
+
+                brushSizePreview.isHidden = false
+
+                brushSizePreview.backgroundColor = viewModel.color
+                updateBrushSizePreview(to: viewModel.width)
+            case .ended:
+                brushSizePreview.isHidden = true
+            default:
+                break
+            }
+        }
     }
 
     public func present(viewModel: PaletteViewModel) {
@@ -49,7 +89,19 @@ public final class PaletteView: UIView, FromNib {
             viewModel.width = sender.value
 
             eventHandler?.modelDidChange(viewModel: viewModel)
+
+            updateBrushSizePreview(to: viewModel.width)
         }
+    }
+
+    func updateBrushSizePreview(to size: Float) {
+        brushSizePreview.snp.updateConstraints { make in
+            make.height.width.equalTo(size)
+            make.right.equalTo(self.snp.left).offset(-16 - (50 - size) / 2)
+            make.bottom.equalTo(self.snp.bottom).offset(-16 - (50 - size) / 2)
+        }
+
+        brushSizePreview.layer.cornerRadius = CGFloat(size / 2)
     }
 
     @IBAction func drawToolSelectedIndexValueChanged(_ sender: UISegmentedControl) {
@@ -90,7 +142,7 @@ extension PaletteView: UICollectionViewDelegate {
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if let viewModel = viewModel, let selectedColor = PaletteView.paletteColors.get(atIndex: indexPath.row) {
             viewModel.color = selectedColor
-            
+
             viewModel.tool = .pencil
             drawToolSegmentedControl.selectedSegmentIndex = 0
 
